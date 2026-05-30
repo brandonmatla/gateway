374a3ce5a94a890401e59fac8cdfde60ee12518b
Brandon Matla <brandonmatla@gmail.com>
2026-05-30 14:49:33 -0600

    cambio

---
+++ src/main/java/com/rag/gateway/config/LoggingFilter.java
@@ -0,0 +1,27 @@
+package com.rag.gateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class LoggingFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange,
+                             GatewayFilterChain chain) {
+
+        log.info("Petición recibida: {} {}",
+                exchange.getRequest().getMethod(),
+                exchange.getRequest().getURI());
+
+        return chain.filter(exchange)
+                .then(Mono.fromRunnable(() ->
+                        log.info("Respuesta: {}",
+                                exchange.getResponse().getStatusCode())));
+    }
+}
